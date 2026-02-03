4b12e08c941f2be4f026d7ef8417c56bbb466c1d
Harshpreet1729 <harshp4466@gmail.com>
2026-02-03 12:19:13 +0530

    Fix .sh

--- start-django.sh
+++ start-django.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-poetry run python manage.py collectstatic --no-input
-poetry run python manage.py migrate
+#!/bin/sh
+set -e
 
-if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn djangoproject.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+if [ "$ENV_STATE" = "production" ]; then
+    gunicorn djangoproject.wsgi:application \
+        --bind 0.0.0.0:8000 \
+        --workers ${GUNICORN_WORKERS:-3}
 else
-    poetry run python manage.py runserver 0.0.0.0:8000
+    python manage.py runserver 0.0.0.0:8000
 fi
